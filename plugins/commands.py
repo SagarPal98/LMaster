fafdf9ff4e2ced51c71bb4ecb270031c6b9671fd
SagarPal98 <earningpoint973@gmail.com>
2025-06-22 11:00:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -370,7 +370,7 @@ async def start(client, message):
     )
     k = await msg.reply(
         f"<b><u>"    " + ❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\n"
-        f"<b>ᴛʜɪꜱ ꜰɪʟᴇ ᴡɪʟʟ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴀꜰᴛᴇʀ {get_time(DELETE_TIME)}.</b> 
+        f"<b>ᴛʜɪꜱ ꜰɪʟᴇ ᴡɪʟʟ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴀꜰᴛᴇʀ {get_time(DELETE_TIME)}.</b>\n" 
         "<b><i>ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪꜱ ꜰɪʟᴇ ᴛᴏ ꜱᴏᴍᴇᴡʜᴇʀᴇ ᴇʟꜱᴇ ᴀɴᴅ ꜱᴛᴀʀᴛ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇʀᴇ</i></b>",
         quote=True
     )     
