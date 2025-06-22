19950f34db62af33ad9ff9e8b1f9ff8e3c7d71d1
SagarPal98 <earningpoint973@gmail.com>
2025-06-22 11:29:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -369,7 +369,7 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(btn)
     )
     k = await msg.reply(
-        f"<pre>     ❗️❗️❗️IMPORTANT❗️️❗️❗️<pre>\n\n"
+        f"<b><u>&nbsp;&nbsp;&nbsp;&nbsp;❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\n"
         f"<b>ᴛʜɪꜱ ꜰɪʟᴇ ᴡɪʟʟ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴀꜰᴛᴇʀ {get_time(DELETE_TIME)}.</b>\n" 
         "<b><i>ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪꜱ ꜰɪʟᴇ ᴛᴏ ꜱᴏᴍᴇᴡʜᴇʀᴇ ᴇʟꜱᴇ ᴀɴᴅ ꜱᴛᴀʀᴛ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇʀᴇ</i></b>",
         quote=True
